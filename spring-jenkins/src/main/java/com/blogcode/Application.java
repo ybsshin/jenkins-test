989edae3583dee600f5d212d217d7a825ecc99f8
ybs.shin <ybs.shin@samsung.com>
2019-12-31 15:05:58 +0900

    init

--- spring-jenkins/src/main/java/com/blogcode/Application.java
+++ spring-jenkins/src/main/java/com/blogcode/Application.java
@@ -1,14 +1,12 @@
-package com.example.demo;
+package com.blogcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("App launched");
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
